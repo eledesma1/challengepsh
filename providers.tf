62a56737f2ff3a932da93d689eea5f792e931966
eledesma <eugenia.ledesma@bitlogic.io>
2024-12-21 16:24:11 -0300

    remocion de credenciales para prueba local

--- providers.tf
+++ providers.tf
@@ -6,17 +6,8 @@ terraform {
     }
   }
   required_version = ">= 1.3.0"
-
-
-  #backend "s3" {
-  #   bucket  = "challenge-states"
-  #   key     = "state/terraform.tfstate" #CAMBIAR EL NOMBRE PARA LA KEY/FOLDER
-  #   region  = "us-east-1"
-  #   profile = "personal"
-  #}
 }
 
 provider "aws" {
   region = "us-east-1"
-  profile = "personal"
 }
