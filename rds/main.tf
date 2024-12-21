2b352a067fbe2e1970e8c327a63133407065df34
eledesma <eugenia.ledesma@bitlogic.io>
2024-12-21 16:22:57 -0300

    ajustes varios

--- rds/main.tf
+++ rds/main.tf
@@ -13,8 +13,8 @@ resource "aws_db_instance" "postgres" {
   engine_version       = "14.9"
   instance_class       = "db.t3.micro"
   db_name              = "challenge_db"
-  username             = chpsh
-  password             = Chall3ng3PSh
+  username             = "chpsh"
+  password             = "Chall3ng3PSh"
   vpc_security_group_ids = var.vpc_security_group_ids
   db_subnet_group_name = aws_db_subnet_group.rds.name
   skip_final_snapshot  = true
@@ -22,8 +22,4 @@ resource "aws_db_instance" "postgres" {
   tags = {
     Name = "challenge-postgres"
   }
-}
-
-data "aws_secretsmanager_secret_version" "postgres_db_secret" {
-  secret_id = aws_secretsmanager_secret.postgres_db_secret.id
 }
